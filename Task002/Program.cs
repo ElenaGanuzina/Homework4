fb34a5dfdc27fe045e49bc56f0869fb0deacffb3
ElenaG <freshelf@yandex.ru>
2022-08-17 11:14:29 +0500

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DigitSum(int number)
+{
+    number = Math.Abs(number);
+    int count = 0;
+    int result = 0;
+    while(number != 0)
+    {
+        result = result + number % 10; 
+        number = number / 10;
+        count++;
+    }
+    return result;
+}
+
+void PrintDigitSum(int number)
+{
+    Console.WriteLine($"Сумма цифр числа {number} равна {DigitSum(number)}");
+}
+
+PrintDigitSum(45);
+PrintDigitSum(147);
+PrintDigitSum(-715);
